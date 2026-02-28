9fadda06f9cc13c0f995a5ba94f84df2c4d06b04
Zixther <duckandfiretto@gmail.com>
2026-03-01 01:02:31 +0200

    fixed docker

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,31 +1,4 @@
-// frontend/src/pages/Home.tsx
-import { useEffect, useState } from "react"
-
-interface Product {
-  id: number
-  name: string
-  price: number
+function Home(){
+    return <h1>Home Page</h1>;
 }
-
-export default function Home() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/products/")
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  return (
-    <div>
-      <h1>Home Page</h1>
-      
-      {products.map(product => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>{product.price}$</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+export default Home
