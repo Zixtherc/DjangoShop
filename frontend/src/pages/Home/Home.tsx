a952a1d5ec48ac2acf3daa42b234b82847a44e67
Zixther <duckandfiretto@gmail.com>
2026-03-07 13:40:49 +0200

    fixed front structure

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,22 @@
+// @ts-ignore
+import React from "react";
+// @ts-ignore
+import { useSmoothScroll } from '../../components/ScrollTo/ScrollTo';
+
+function Home() {
+
+    return(
+        <div className="intoduce">
+        <h1>Home Page</h1>
+        <h1>QUANTUM {"{FIELDS}"}</h1>
+        <h1>{'{FLUX}'}</h1>
+        <h1>::[0110]::</h1>
+        <h1>Σ</h1>
+        <h1>𝝅</h1>
+        </div>
+
+    )
+
+}
+
+export default Home
